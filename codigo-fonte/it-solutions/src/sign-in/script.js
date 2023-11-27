c58403a3b9999a78ebbcd470616c3476f6f2901d
DanielaFCruz <dannifndez@outlook.com>
2023-11-26 22:11:39 -0300

    inclusão dos scipts atualizados

--- codigo-fonte/it-solutions/src/sign-in/script.js
+++ codigo-fonte/it-solutions/src/sign-in/script.js
@@ -1,38 +1,15 @@
 <script>
-    document.getElementById('loginForm').addEventListener('submit', function(event) {
-        event.preventDefault(); // Previne o envio padrão do formulário
+        function attemptLogin() {
+            var username = document.getElementById('usuario').value;
+            var password = document.getElementById('password').value;
 
-    // Obter valores dos campos de entrada
-    var username = document.getElementById('usuario').value;
-    var password = document.getElementById('password').value;
-
-    // Criar objeto com os dados do formulário
-    var formData = {
-        username: username,
-    password: password
-    };
-
-    // Enviar uma solicitação POST para o servidor (substitua 'url_do_seu_servidor' pela URL real)
-    fetch('url_do_seu_servidor', {
-        method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-        },
-    body: JSON.stringify(formData)
-    })
-    .then(response => response.json())
-    .then(data => {
-        // Processar a resposta do servidor
-        if (data.authenticated) {
-        console.log('Autenticação bem-sucedida!');
-            // Redirecionar para a página principal ou fazer outras ações necessárias
-        } else {
-        console.log('Falha na autenticação. Verifique suas credenciais.');
-            // Exibir uma mensagem de erro ao usuário ou tomar outras medidas necessárias
+            // Simulação de autenticação local
+            if (username === 'seu_usuario' && password === 'sua_senha') {
+                var userObject = { username: username, authenticated: true };
+                localStorage.setItem('currentUser', JSON.stringify(userObject));
+                window.location.href = '../home/index.html'; // Redirecionar para a página principal
+            } else {
+                alert('Falha na autenticação. Verifique suas credenciais.');
+            }
         }
-    })
-    .catch(error => {
-        console.error('Erro na solicitação:', error);
-    });
-
-</script>
+    </script>
